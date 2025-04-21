85dd180164c8eeaa0fd8fa0678dfe725d16135db
Rodrigo Oliveira <rodrigo.silva.olviera369@gmail.com>
2025-04-21 00:27:33 -0300

    Update swiper styles and navigation for improved layout and responsiveness

--- scripts/swiperCards.js
+++ scripts/swiperCards.js
@@ -9,17 +9,13 @@ function initSwiperCards() {
             centeredSlides: true,
             loop: true,
             loopFillGroupWithBlank: true,
-            pagination: {
-                el: ".swiper-pagination",
-                clickable: true,
-            },
             navigation: {
                 nextEl: ".swiper-button-next",
                 prevEl: ".swiper-button-prev",
             },
             breakpoints: {
-                768: {slidesPerView: 3, spaceBetween: 20},
-                1440: {slidesPerView: 4, spaceBetween: 20}
+                768: {slidesPerView: 3, spaceBetween: 10},
+                1440: {slidesPerView: 5, spaceBetween: 10}
             }
         });
     }
